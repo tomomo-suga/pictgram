c88ba90eb53dc58468afd5113c3f119bb68549a6
TomomiSugawara <t.mom.505@gmail.com>
2024-11-07 17:29:34 +0900

    first commit

---
+++ src/main/java/com/example/PictgramApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PictgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PictgramApplication.class, args);
+	}
+
+}
